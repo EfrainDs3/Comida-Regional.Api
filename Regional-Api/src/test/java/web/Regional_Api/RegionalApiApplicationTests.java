12704b5c8a25e677337341eaa509e9b8926745be
EfrainDs3 <santiagotorresmurrieta@gmail.com>
2025-10-25 10:21:43 -0500

    Primer Commit

---
+++ Regional-Api/src/test/java/web/Regional_Api/RegionalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package web.Regional_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
